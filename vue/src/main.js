b1ed79bbc21c98c5750bb69f9efee7b8506cd9d1
[yun] <andylow@yeah.net>
2021-04-19 22:06:23 +0800

    Update dependencies.

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueTelInput from "vue-tel-input";
 import VueMobileDetection from "vue-mobile-detection";
 import App from './App.vue'
+import 'vue-tel-input/dist/vue-tel-input.css'
 
 Vue.use(VueTelInput);
 Vue.use(VueMobileDetection);
